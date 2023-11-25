d84dc37850a860f63853e2bcc7ae64368992f1ea
AmitRaheja-co <77236208+AmitRaheja-co@users.noreply.github.com>
2023-11-25 16:38:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const info = require("./models/info");
+const INFO = require("./models/info");
 const bodyParser = require('body-parser');
+const info = require("./models/info");
 
 const apiKey = '3701c28b1ac01a9e76bb88a56ee8201b';
 // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -17,32 +18,55 @@ const port = 7000;
 
 
 app.get("/", async (req, res) => {
-    const Info = await info.find({});
-    console.log(Info);
-    res.render('main', { info: Info[0] });
+    const Info = await INFO.find({});
+    if (Info.length) {
+        console.log(Info);
+        console.log("in get");
+        console.log(Info[0].city);
+        return res.render('main', { info: Info[0] });
+    }
+    console.log("No entry in database");
+    res.render('main', {
+        info: {
+            city: "enter city",
+            weather: "NULL"
+        }
+    });
 });
 
 
 app.post("/", async (req, res) => {
-    if(!req.body.city){
-        try{
-            const Info = await info.find({});
-            res.render('main', { info: Info });
-        }   
-        catch(err){
+    if (!req.body.city) {
+        try {
+            const Info = await INFO.find({});
+            if (Info.length) {
+                console.log(Info);
+                console.log("in get");
+                console.log(Info[0].city);
+                return res.render('main', { info: Info[0] });
+            }
+            console.log("No entry in database");
+            res.render('main', {
+                info: {
+                    city: "enter city",
+                    weather: "NULL"
+                }
+            });
+        }
+        catch (err) {
             console.log('Error in fetching info: ', err);
             return res.status(500).send('Internal Server Error');
-        } 
+        }
     }
-    try{
-        await info.deleteMany({}); // Delete all documents in the info collection
+    try {
+        await INFO.deleteMany({}); // Delete all documents in the info collection
         console.log('Deleted all info');
         const Weather = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${apiKey}&units=metric`)).json();
         console.log(Weather);
-        const new_info = await info.create({
-            city:req.body.city,
-            weather:Weather.main.temp
-        }); 
+        const new_info = await INFO.create({
+            city: req.body.city,
+            weather: Weather.main.temp
+        });
         console.log(new_info);
         console.log("changed city");
         res.render('main', { info: new_info });
